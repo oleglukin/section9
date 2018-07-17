7be20a3a33be5bf5c6db0975087cdae27839bbcc
oleglukin <oleglukin@yahoo.com>
2018-07-17 22:56:52 +1000

    Updated index and server.js for Graph QL

--- graphql-api/src/index.js
+++ graphql-api/src/index.js
@@ -1,8 +1,5 @@
 import setupGraphQLServer from "./graphql/server"
-import { https } from "firebase-functions"
 
-/* CF for Firebase with graphql-server-express */
 const graphQLServer = setupGraphQLServer()
 
-// https://us-central1-section9-210115.cloudfunctions.net/api
-export const api = https.onRequest(graphQLServer)
+graphQLServer.listen(80, () => console.log('GraphQL server listening on port 80'))
