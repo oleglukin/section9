75f79a24eefcc71994c8a6a4e637649b9bf4c7b1
oleglukin@yahoo.com <oleglukin@yahoo.com>
2018-07-21 23:13:45 +1000

    Project structure

--- graphql-api/src/data/resolvers.js
+++ graphql-api/src/data/resolvers.js
@@ -1,15 +1,7 @@
 import firebase from 'firebase'
+import firebaseConfig from '../config'
 
-// Initialise Firebase with a config
-var config = {
-  apiKey: "BTRaS45950q6FRKdoLzgtr45hm11RXnQT7GaCHo",
-  authDomain: "section9-210115.firebaseapp.com",
-  databaseURL: "https://section9-210115.firebaseio.com",
-  projectId: "section9-210115",
-  storageBucket: "section9-210115.appspot.com",
-  messagingSenderId: "124504813524"
-};
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 
 var fligthsParent = "/jan-2018-limited/";
 
